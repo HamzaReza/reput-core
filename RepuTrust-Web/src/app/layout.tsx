0ddfa0a04a4e1055e729e38ac2ba84da3c9a7cbb
HamzaReza <rezahamza5@gmail.com>
2026-05-06 14:33:03 +0500

    speed insights

--- RepuTrust-Web/src/app/layout.tsx
+++ RepuTrust-Web/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -41,6 +42,7 @@ export default function RootLayout({
         }}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
